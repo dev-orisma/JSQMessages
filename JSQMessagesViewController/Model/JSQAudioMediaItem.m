addb8f29479bf732123bf516bc5dfa3f4b1b8162
Auttaporn Feungngern <auttaporn@orisma.com>
2017-04-07 16:15:42 +0700

    update

--- JSQMessagesViewController/Model/JSQAudioMediaItem.m
+++ JSQMessagesViewController/Model/JSQAudioMediaItem.m
@@ -145,24 +145,32 @@
 
 - (NSString *)timestampString:(NSTimeInterval)currentTime forDuration:(NSTimeInterval)duration
 {
-    // print the time as 0:ss or ss.x up to 59 seconds
-    // print the time as m:ss up to 59:59 seconds
-    // print the time as h:mm:ss for anything longer
-    if (duration < 60) {
-        if (self.audioViewAttributes.showFractionalSeconds) {
-            return [NSString stringWithFormat:@"%.01f", currentTime];
-        }
-        else if (currentTime < duration) {
-            return [NSString stringWithFormat:@"0:%02d", (int)round(currentTime)];
-        }
-        return [NSString stringWithFormat:@"0:%02d", (int)ceil(currentTime)];
+    if (duration < 1) {
+        return [NSString stringWithFormat:@"00:01"];
     }
-    else if (duration < 3600) {
-        return [NSString stringWithFormat:@"%d:%02d", (int)currentTime / 60, (int)currentTime % 60];
-    }
-    
-    return [NSString stringWithFormat:@"%d:%02d:%02d", (int)currentTime / 3600, (int)currentTime / 60, (int)currentTime % 60];
+    return [NSString stringWithFormat:@"%02d:%02d", (int)currentTime / 60, (int)currentTime % 60];
 }
+//
+//- (NSString *)timestampString:(NSTimeInterval)currentTime forDuration:(NSTimeInterval)duration
+//{
+//    // print the time as 0:ss or ss.x up to 59 seconds
+//    // print the time as m:ss up to 59:59 seconds
+//    // print the time as h:mm:ss for anything longer
+//    if (duration < 60) {
+//        if (self.audioViewAttributes.showFractionalSeconds) {
+//            return [NSString stringWithFormat:@"%.01f", currentTime];
+//        }
+//        else if (currentTime < duration) {
+//            return [NSString stringWithFormat:@"0:%02d", (int)round(currentTime)];
+//        }
+//        return [NSString stringWithFormat:@"0:%02d", (int)ceil(currentTime)];
+//    }
+//    else if (duration < 3600) {
+//        return [NSString stringWithFormat:@"%d:%02d", (int)currentTime / 60, (int)currentTime % 60];
+//    }
+//    
+//    return [NSString stringWithFormat:@"%d:%02d:%02d", (int)currentTime / 3600, (int)currentTime / 60, (int)currentTime % 60];
+//}
 
 - (void)onPlayButton:(UIButton *)sender
 {
